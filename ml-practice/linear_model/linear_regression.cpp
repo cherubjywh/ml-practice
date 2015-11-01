861986fb3e35b164a30d4f38ebfc6741c9f3a7fe
Yuyin Sun <cherubjywh@gmail.com>
2015-11-01 11:05:57 -0800

    linear_regression

---
+++ ml-practice/linear_model/linear_regression.cpp
@@ -0,0 +1,9 @@
+//
+//  linear_regression.cpp
+//  ml-practice
+//
+//  Created by Yuyin Sun on 15-11-1.
+//  Copyright (c) 2015年 Yuyin Sun. All rights reserved.
+//
+
+#include "linear_regression.h"
---
+++ ml-practice/linear_model/linear_regression.h
@@ -0,0 +1,14 @@
+//
+//  linear_regression.h
+//  ml-practice
+//
+//  Created by Yuyin Sun on 15-11-1.
+//  Copyright (c) 2015年 Yuyin Sun. All rights reserved.
+//
+
+#ifndef __ml_practice__linear_regression__
+#define __ml_practice__linear_regression__
+
+#include <stdio.h>
+
+#endif /* defined(__ml_practice__linear_regression__) */
