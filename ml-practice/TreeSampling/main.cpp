7f53bb737375683f76b7b64d0a7ec2f21b62c149
Yuyin Sun <cherubjywh@gmail.com>
2015-11-17 23:43:36 -0800

    Dont have time, just keep on streak

--- ml-practice/TreeSampling/main.cpp
+++ ml-practice/TreeSampling/main.cpp
@@ -22,13 +22,13 @@ using namespace std;
 
 class multinomial {
     public:
-    static size_t get_one(const vector<double>::iterator iter_begin, const vector<double>::iterator iter_end) {
+    static int get_one(const vector<double>::iterator iter_begin, const vector<double>::iterator iter_end) {
         boost::random::discrete_distribution<> dist(iter_begin, iter_end);
         return dist(*(multinomial::rng));
     }
-    static void get_multiple(const vector<double>::iterator iter_begin, const vector<double>::iterator iter_end, int N, unordered_map<size_t, int>& results) {
+    static void get_multiple(const vector<double>::iterator iter_begin, const vector<double>::iterator iter_end, int N, unordered_map<int, int>& results) {
         for (int i = 0; i < N; ++i) {
-            size_t num = get_one(iter_begin, iter_end);
+            int num = get_one(iter_begin, iter_end);
             if (results.find(num) == results.end()) {
                 results[num] = 1;
             } else {
@@ -46,35 +46,48 @@ auto_ptr<boost::mt19937> multinomial::rng(new boost::mt19937());
 class tree_sampler {
 public:
     tree_sampler(const vector<vector<double>>& weights);
-    void sample_a_tree(unordered_map<size_t, size_t> &);
-    void sample_trees(size_t, vector<unordered_map<size_t, size_t>>&);
-    void sample_trees(size_t, string);
+    void sample_a_tree(unordered_map<int, int> &);
+    void sample_trees(int, vector<unordered_map<int, int>>&);
+    void sample_trees(int, string) {}
 private:
     vector<vector<double>> _weights;
 };
 
 tree_sampler::tree_sampler(const vector<vector<double>>& weights) {
     _weights = weights;
+    for (auto i = 0; i < _weights.size(); ++i) {
+        double total = 0.0;
+        for (auto j = 0; j < _weights[0].size(); ++j) {
+            total += _weights[i][j];
+        }
+        for (auto j = 0; j < _weights[0].size(); ++j) {
+            _weights[i][j] /= total;
+        }
+    }
 }
 
-void tree_sampler::sample_trees(size_t N, vector<unordered_map<size_t, size_t> > &trees) {
-    for (size_t i = 0; i < N; ++i) {
+
+
+void tree_sampler::sample_trees(int N, vector<unordered_map<int, int> > &trees) {
+    for (int i = 0; i < N; ++i) {
+        trees.push_back(unordered_map<int, int>());
         sample_a_tree(trees[i]);
     }
 }
 
-void tree_sampler::sample_a_tree (unordered_map<size_t, size_t> &tree) {
-    set<size_t> in_tree;
+void tree_sampler::sample_a_tree (unordered_map<int, int> &tree) {
+    set<int> in_tree;
     in_tree.insert(0);
     
-    for (size_t i = 1; i < _weights.size(); ++i) {
-        size_t n = i;
-        set<size_t> temp;
+    for (int i = 1; i < _weights.size(); ++i) {
+        int n = i;
+        set<int> temp;
         while (in_tree.find(n) == in_tree.end()) {
             temp.insert(n);
             vector<double> pvals = _weights[n - 1];
-            size_t u = multinomial::get_one(pvals.begin(), pvals.end());
+            int u = multinomial::get_one(pvals.begin(), pvals.end());
             tree[n] = u;
+
             n = u;
         }
         
@@ -92,7 +105,7 @@ int main(int argc, const char * argv[]) {
     
     cout << multinomial::get_one(pvals.begin(), pvals.end()) << endl;
     
-    unordered_map<size_t, int> results;
+    unordered_map<int, int> results;
     int N = 10000;
     multinomial::get_multiple(pvals.begin(), pvals.end(), N, results);
     
@@ -103,6 +116,14 @@ int main(int argc, const char * argv[]) {
             cout << i << ": " << results[i]*1.0/N << endl;
         }
     }
+    
+    vector<vector<double>> weights = {{0.1, 0, 0.3, 0.3}, {0.2, 0.4, 0, 0.4}, {0.4, 0.2, 0.1, 0}};
+    
+    tree_sampler ts(weights);
+    
+    vector<unordered_map<int, int>> trees;
+    
+    ts.sample_trees(10, trees);
     
     return 0;
 }
